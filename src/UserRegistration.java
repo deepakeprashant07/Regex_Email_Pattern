084429c1edb72afbdf44d6ed19c881fd4dd87a10
Prashant Deepake <deepakeprashant1@gmail.com>
2022-06-08 00:14:56 +0530

    ADDED : LAST NAME VALIDATION PROGRAM

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -4,15 +4,15 @@ public class UserRegistration {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter valid first name");
-            String firstName = scan.next();
+            System.out.println("Enter valid Last name");
+            String lastName = scan.next();
             String regrex = "^[A-Z][a-z]{3,9}$";
-            boolean result = firstName.matches(regrex);
+            boolean result = lastName.matches(regrex);
             if (result) {
-                System.out.println(firstName + " First Name is valid");
+                System.out.println(lastName + " Last Name is valid");
                 break;
             } else {
-                System.out.println("Name Does not valid Example : Viky");
+                System.out.println("Name Does not valid Example : Deepake");
             }
         }
     }
